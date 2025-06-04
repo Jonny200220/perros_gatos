ac835e148b20b7c52d1d301955a93b82e9897c45
Jonny Ramirez <jonnyrmzhdz@gmail.com>
2025-06-04 13:18:50 -0600

    Actualizacion del main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
+
 import tensorflow as tf
 from sklearn.metrics import confusion_matrix, classification_report
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -8,13 +6,12 @@ from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
 from tensorflow.keras.models import Model
 
-# 1. Parámetros
+# Parámetros
 DATASET_DIR = './dataset'  # <- cambia esto a la ruta real
 IMG_SIZE = (160, 160)
 BATCH_SIZE = 32
 SEED = 42
 
-# 2. Crear generadores de datos (80/20 aleatorio)
 train_datagen = ImageDataGenerator(
     preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input,
     validation_split=0.2
@@ -39,7 +36,7 @@ val_generator = train_datagen.flow_from_directory(
     shuffle=False  # Para evaluar luego
 )
 
-# 3. Cargar MobileNetV2 sin la parte superior
+# Cargar MobileNetV2 sin la parte superior
 base_model = MobileNetV2(input_shape=IMG_SIZE + (3,), include_top=False, weights='imagenet')
 base_model.trainable = False  # 4. Congelar todas las capas del modelo base
 
@@ -51,15 +48,13 @@ predictions = Dense(1, activation='sigmoid')(x)  # 2 clases: salida binaria
 
 model = Model(inputs=base_model.input, outputs=predictions)
 
-# 6. Compilar y entrenar solo el head
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 model.fit(train_generator, epochs=5, validation_data=val_generator)
 
-# 7. Evaluar con conjunto de prueba
+# Evaluar con conjunto de prueba
 loss, accuracy = model.evaluate(val_generator)
 print(f"\nPrecisión en validación: {accuracy:.4f}")
 
-# 8. Matriz de confusión y reporte
 y_true = val_generator.classes
 y_pred_prob = model.predict(val_generator)
 y_pred = (y_pred_prob > 0.5).astype(int).flatten()
